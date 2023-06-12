d9f798b3d1433e44ba6dd6e8b18c812d5f5e6c34
Taejun Kim <89377440+TAEJUN1293@users.noreply.github.com>
2023-06-12 13:37:47 +0900

    Update RestcountriesAPItoDAG.py

--- RestcountriesAPItoDAG.py
+++ RestcountriesAPItoDAG.py
@@ -18,6 +18,7 @@ def get_Redshift_connection(autocommit=True):
 
 @task
 def extract(url):
+    # 추출 시점 로깅입력
     logging.info(datetime.utcnow())
     response = requests.get(url)
     data = json.loads(response.text)
@@ -31,6 +32,7 @@ def transform(extract_data):
         population = data["population"]
         area = data["area"] 
         records.append([country, population, area])
+    # transform 끝난 것 확인 
     logging.info("Data Transform finished")
     return records
 
@@ -40,23 +42,29 @@ def load(schema, table, records):
     cur = get_Redshift_connection()
     try:
         cur.execute("BEGIN;")
+        # 테이블 있으면 삭제 실행
         cur.execute(f"DROP TABLE IF EXISTS {schema}.{table};")
+        # 테이블 생성
         cur.execute(f"""
         CREATE TABLE {schema}.{table} (
             country varchar(50),
             population bigint,
             area float
         );""")
+        # 모든 record에 대해 삽입 작업 실행
         for record in records:
             sql = f"INSERT INTO {schema}.{table} VALUES (%s, %s, %s);"
             cur.execute(sql, record)
         cur.execute("COMMIT;")   
+    # 에러 발생 시 예외 조건, 롤백 처리
     except Exception as error:
         print(error)
         cur.execute("ROLLBACK;")
         raise
+    # load 작업 완료 확인
     logging.info("Load finished")
 
+# 기본 인자 입력
 default_args = {
     'owner': 'taejun3305',
     'email': ['taejun3305@gmail.com'],
@@ -64,6 +72,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+# DAG 생성
 with DAG(
     dag_id='UpdateCountriesInfo',
     start_date=datetime(2023, 6, 9),
